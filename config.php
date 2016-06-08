025887d9fbbd054214580c47f1b5dc43b397d285
tehsunnliu@gmail.com <tehsunnliu@gmail.com>
2016-06-08 05:43:09 +0530

    OpenCartTest

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost:19366/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost:19366/');
+
+// DIR
+define('DIR_BASE', realpath($_SERVER['DOCUMENT_ROOT']) . '/');
+define('DIR_APPLICATION', DIR_BASE . 'catalog/');
+define('DIR_SYSTEM', DIR_BASE . 'system/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_IMAGE', DIR_BASE . 'image/');
+define('DIR_CACHE', DIR_SYSTEM . 'cache/');
+define('DIR_DOWNLOAD', DIR_SYSTEM . 'download/');
+define('DIR_UPLOAD', DIR_SYSTEM . 'upload/');
+define('DIR_MODIFICATION', DIR_SYSTEM . 'modification/');
+define('DIR_LOGS', DIR_SYSTEM . 'logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'ocartuser487');
+define('DB_PASSWORD', 'HKaJZw6K*^PJ');
+define('DB_DATABASE', 'opencartdb487');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
